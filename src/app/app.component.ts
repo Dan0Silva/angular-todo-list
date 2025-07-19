36700c1747e271d1e2811e868d296183e7cea097
Dan0Silva <dan0sillva@gmail.com>
2025-07-19 16:08:46 -0300

    feat: add Storybook and create example Button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { SharedModule } from './shared/shared.module';
+import { FeaturesModule } from './features/features.module';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'todo_list';
-}
+export class AppComponent {}
