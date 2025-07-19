5f5ae9eef521246875e55d36ebaf519a340b79fe
Dan0Silva <dan0sillva@gmail.com>
2025-07-19 18:01:15 -0300

    refactor: add material icons in the index.html and update shared module imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ButtonComponent, HeaderComponent, SearchBarComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
