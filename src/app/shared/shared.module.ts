9dd08b9e07ab97d65db4dfac4758a55a6dda81c4
Dan0Silva <dan0sillva@gmail.com>
2025-07-20 10:00:24 -0300

    refactor: remove button component declaration from shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
-  declarations: [
-    ButtonComponent,
-    HeaderComponent,
-    SearchBarComponent,
-    TaskCardComponent,
-  ],
+  declarations: [HeaderComponent, SearchBarComponent, TaskCardComponent],
   exports: [HeaderComponent, TaskCardComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
