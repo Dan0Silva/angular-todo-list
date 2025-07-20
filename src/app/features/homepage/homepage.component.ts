b31bb9b884eec914ca338d0bf1db6c97ad1899f1
Dan0Silva <dan0sillva@gmail.com>
2025-07-19 23:01:37 -0300

    feat: add task list a into a homepage

--- src/app/features/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../../core/services/todo/todo.service';
+import Task from '../../core/interfaces/task';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
+  tasks: Task[] = [];
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit() {
+    this.tasks = this.todoService.read();
+  }
 }
