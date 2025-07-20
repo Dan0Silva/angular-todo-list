4e29a78e319a274f71a199e017bbdef78eef6019
Dan0Silva <dan0sillva@gmail.com>
2025-07-19 23:02:34 -0300

    fix: adjust declarations and exports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,16 @@ import { ButtonComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TaskCardComponent } from './components/task-card/task-card.component';
 
 @NgModule({
-  declarations: [ButtonComponent, HeaderComponent, SearchBarComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    ButtonComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    TaskCardComponent,
+  ],
+  exports: [HeaderComponent, TaskCardComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
