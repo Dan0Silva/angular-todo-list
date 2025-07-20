d5801abc0a926321e00d54cdfddfa792a17f0885
Dan0Silva <dan0sillva@gmail.com>
2025-07-20 11:16:40 -0300

    feat: persist tasks in localStorage through todoService

--- src/app/core/services/todo/todo.service.ts
+++ src/app/core/services/todo/todo.service.ts
@@ -6,12 +6,19 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class TodoService {
+  private readonly STORAGE_KEY = 'todo-list';
   private taskList: Task[] = [];
-  private taskSubject = new BehaviorSubject<Task[]>([]);
+  private taskSubject = new BehaviorSubject<Task[]>(
+    this.loadTasksInLocalStorage()
+  );
 
   task$: Observable<Task[]> = this.taskSubject.asObservable();
 
   constructor() {}
+
+  getTasks(): Task[] {
+    return this.taskSubject.getValue();
+  }
 
   create(description: string): void {
     const newTask: Task = {
@@ -20,21 +27,36 @@ export class TodoService {
       status: false,
     };
     this.taskList = [...this.taskList, newTask];
+
     this.emitTaskList();
+    this.saveTasksInLocalStorage(this.taskList);
   }
 
   delete(idToRemove: number): void {
     this.taskList = this.taskList.filter((item) => item.id !== idToRemove);
+
     this.emitTaskList();
+    this.saveTasksInLocalStorage(this.taskList);
   }
 
   toggleTask(id: number): void {
     const task = this.taskList.find((t) => t.id === id);
     if (task) {
       task.status = !task.status;
+
       this.emitTaskList();
+      this.saveTasksInLocalStorage(this.taskList);
     }
     console.log(this.taskList);
+  }
+
+  private loadTasksInLocalStorage(): Task[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+  private saveTasksInLocalStorage(tasks: Task[]) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(tasks));
   }
 
   private emitTaskList(): void {
