3467555104567ce04b9b386f892b7f9ed2b69bb7
Dan0Silva <dan0sillva@gmail.com>
2025-07-20 16:57:23 -0300

    feat: create load-spinner component

---
+++ src/app/shared/components/load-spinner/load-spinner.stories.ts
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/angular';
+import { LoadSpinnerComponent } from './load-spinner.component';
+
+const meta: Meta<LoadSpinnerComponent> = {
+  title: 'components/LoadSpinner',
+  component: LoadSpinnerComponent,
+};
+export default meta;
+
+type Story = StoryObj<LoadSpinnerComponent>;
+
+export const Default: Story = {};
